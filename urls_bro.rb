7d6c0e8f71ccecbd1f94a85ee507fbe895e660ec
Adam Price <komidore64@gmail.com>
2014-06-30 15:46:21 -0400

    first functions iteration of urls_bro.rb

--- urls_bro.rb
+++ urls_bro.rb
@@ -1,30 +1,62 @@
-require 'net/https'
-require 'uri'
+TITLE       = "urls_bro"
+AUTHOR      = "komidore64"
+VERSION     = "0.0.1"
+LICENSE     = "GPL3"
+DESCRIPTION = "a very simple url shortener using Red Hat's internal shortener"
 
-URL = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
+
+URL_REGEX = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/
 
 SHORTENER = "https://url.corp.redhat.com/new?%s"
 
-def web_request(uri)
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-  request = Net::HTTP::Get.new(uri.request_uri)
+require 'net/https'
+require 'uri'
 
-  response = http.request(request)
-  response.body
-end
+def urls_bro(data, buffer, date, tags, visible, highlight, nick, message)
+  urls = message.scan(URL_REGEX).collect { |url| url[0] }
 
-def shorten(url)
-  web_request(URI.parse(SHORTENER % url))
-end
-
-def shorten_urls_in_message(message)
-  urls = message.scan(URL).collect { |url| url[0] }
   Thread.new do
+
+    # this is for debug output
+    #%w(data buffer date tags visible highlight nick message).each do |name|
+    #  v = eval(name)
+    #  Weechat.print("", "urls_bro \t#{name}: #{v.inspect}")
+    #end
+
     urls.each do |url|
-      puts shorten(url)
+      uri = URI.parse(SHORTENER % url)
+
+      http = Net::HTTP.new(uri.host, uri.port)
+
+      # ssl shenanigans
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      # not sure if this works?
+      http.read_timeout = 10 #seconds
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+
+      begin
+      response = http.request(request)
+      Weechat.print(buffer, "==> #{response.body}")
+      rescue
+        err_url = url.slice(0, 20)
+        err_url[17..19] = "..." if url.length > 20
+        Weechat.print(buffer, "==> error retrieving url for #{err_url}")
+      end
     end
+
   end unless urls.empty?
+
+  return Weechat::WEECHAT_RC_OK
+end
+
+def weechat_init
+  Weechat.register(TITLE, AUTHOR, VERSION, LICENSE, DESCRIPTION, "", "")
+
+  Weechat.hook_print("", "irc_privmsg", "", 1, "urls_bro", "")
+
+  return Weechat::WEECHAT_RC_OK
 end
